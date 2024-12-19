de0cb35bf083bf88669a290333a2148a4379a379
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-12-19 13:10:00 -0300

    add gitignore

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from playwright.sync_api import sync_playwright
 
+DEBUG = True
 
 def get_all_percentages_from_page(url) -> list:
     try:
@@ -21,6 +22,30 @@ def get_all_percentages_from_page(url) -> list:
         print(f"An error occurred: {e}")
         return []
 
+def get_all_opens_from_page(url) -> list:
+    try:
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=False)
+            page = browser.new_page()
+            page.goto(url)
+            page.wait_for_load_state('load')
+            html = page.content()
+            soup = BeautifulSoup(html, 'html.parser')
+            divs = soup.find_all('div', class_='form-guide-overview-insight-card__content-item')
+
+            open_values = []
+
+            for div in divs:
+                spans = div.find_all('span')
+                if spans and spans[0].text.strip() == "Open":
+                    open_values.append(spans[1].text.strip())
+
+            return open_values
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+
 def scrape_race_info():
     # URL of the race
     url = "https://www.punters.com.au/form-guide/horses/bendigo-20241203/apiam-animal-health-mdn-plate-race-1/"
@@ -35,8 +60,14 @@ def scrape_race_info():
 
         # Parse the HTML content
         soup = BeautifulSoup(response.text, 'html.parser')
+
+        # No inicio da loop uma linha e pulada por cause do header da tabela
         percentages = get_all_percentages_from_page(url)
-        print(percentages)
+        open_val = get_all_opens_from_page(url)
+
+        if DEBUG:
+            print(percentages)
+            print(open_val)
 
 
         # Dictionary to store all page information
@@ -91,7 +122,6 @@ def scrape_race_info():
                         flucs_element = cell.select_one('.common-flucs-graph__percentage')
                         if counter < len(percentages):
                             horse_data['flucs_percentage'] = percentages[counter]
-                        counter = counter + 1
 
                         # Captura do valor de Open
                         open_element = cell.select_one('.form-guide-overview-insight-card__content-item')
@@ -111,7 +141,8 @@ def scrape_race_info():
                             horse_data['Rating'] = rating_element.text.strip()
 
                         # Verificação de dados capturados
-                        print(horse_data)  # Adicione esta linha para depuração
+                        if DEBUG:
+                            print("horse_data")
 
                         # Get all text content including nested elements
                         cell_content = cell.get_text(separator=' | ', strip=True)
@@ -124,6 +155,8 @@ def scrape_race_info():
                         links = cell.find_all('a')
                         if links:
                             horse_data[f'links_{i}'] = [{'text': a.text.strip(), 'href': a.get('href', '')} for a in links]
+
+                    counter = counter + 1
 
                     if horse_data:
                         page_data['horse_information'].append(horse_data)
